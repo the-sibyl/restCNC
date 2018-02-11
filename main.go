58a6562693110189c5741290436570972e91d65a
Default Default <default@default.default>
2018-02-11 08:03:42 +0000

    This code successfully communicates over I2C and sets two voltages. The test platform is a Raspberry Pi Zero with a 400kHz dtparam on i2c-1. The DAC is an Adafruit MCP4725 breakout. I need to reset the EEPROM back to datasheet specs. It sppears that the power-on output voltage is half the voltage rail, and it should be zero.

--- main.go
+++ main.go
@@ -25,8 +25,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
+	"golang.org/x/exp/io/i2c"
 	"github.com/gorilla/mux"
+	"github.com/the-sibyl/sysfsGPIO"
 )
 
 // A struct with tags. The Name and one of the Value fields are required. This is a quick and dirty implementation.
@@ -37,6 +40,34 @@ type SpindleRPM struct {
 var spindleRPM SpindleRPM
 
 func main() {
+	// Enable the amber LED
+	gpio22, _ := sysfsGPIO.InitPin(22, "out")
+	defer gpio22.ReleasePin()
+	// LED is wired for active low
+	gpio22.SetLow()
+
+	// Set up I2C
+	// The LSB of the I2C address (A0) is configured from GPIO4. Set it low to make A0=0.
+	gpio4, _ := sysfsGPIO.InitPin(4, "out")
+	defer gpio4.ReleasePin()
+	gpio4.SetLow()
+	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x62)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		gpio22.SetHigh()
+		err = d.Write([]byte{0x8, 0x0, 0x8, 0x0})
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Second * 2)
+		gpio22.SetLow()
+		d.Write([]byte{0xF, 0xFF, 0xF, 0xFF})
+		time.Sleep(time.Second * 2)
+	}
+
 	spindleRPM.Value = -12345
 
 	router := mux.NewRouter().StrictSlash(true)
